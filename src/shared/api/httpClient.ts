96e1b18280e852b28f37c2e93e3df2e53964d69b
qwertydodo <qwertydodo93@gmail.com>
2026-06-09 13:21:16 +0200

    feat: add env config and axios HTTP client with API key interceptor

---
+++ src/shared/api/httpClient.ts
@@ -0,0 +1,24 @@
+import axios from 'axios'
+import { env } from '../config/env'
+import { clearApiKey, getApiKey } from '../lib/storage'
+
+export const httpClient = axios.create({
+  baseURL: env.catApiBaseUrl,
+})
+
+httpClient.interceptors.request.use((config) => {
+  const key = getApiKey()
+  if (key) config.headers['x-api-key'] = key
+  return config
+})
+
+httpClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (axios.isAxiosError(error) && error.response?.status === 401) {
+      clearApiKey()
+      window.location.href = '/auth'
+    }
+    return Promise.reject(error)
+  }
+)
---
+++ src/shared/config/env.ts
@@ -0,0 +1,3 @@
+export const env = {
+  catApiBaseUrl: import.meta.env.VITE_CAT_API_BASE_URL as string,
+} as const
