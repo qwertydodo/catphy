6447600eb22ffaeb8d729a66fe099925510256a6
qwertydodo <qwertydodo93@gmail.com>
2026-06-09 13:19:15 +0200

    feat: add design token system (primitive palette + semantic tokens)

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './shared/ui/tokens.css'
+import { App } from './app/App'
 
 const rootEl = document.getElementById('root')
 if (!rootEl) throw new Error('Root element not found')
